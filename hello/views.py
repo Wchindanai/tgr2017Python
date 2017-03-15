d1c65e36c6ae955f607d66bd2bce9269b66b499d
tanakorn.W <58460703006@dpu.ac.th>
2017-03-15 10:14:55 +0700

    test

--- hello/views.py
+++ hello/views.py
@@ -6,11 +6,31 @@ from .models import Greeting
 import requests
 
 # Create your views here.
+
+dbname 
+user
+host
+password
 def index(request):
-    r= requests.get('https://github.com/timeline.json')
-    print r.text
-    return HttpResponse('<pre>'+r.text+'</pre>')
-    #return render(request, 'index.html')
+    try:
+    conn = psycopg2.connect("dbname='ds7gk4tphvn2v' user='sfszejfztqdxyo' host='ec2-23-21-220-23.compute-1.amazonaws.com' password='9e2b53c8d90e3f9ad12e7764ac7ecee875724c6d2f1d3ae3d31517ac4d88bddc'")
+    cur = conn.cursor()
+
+    sql_command =""" 
+    CREATE TABLE iotdpu_data(
+rank  integer NOT NULL,
+firstname  varchar(40) NOT NULL,
+lastname   varchar(40) NOT NULL,
+university  varchar(40) NOT NULL,
+degree   integer NOT NULL,
+department   varchar(40) NOT NULL,
+gpa   double NOT NULL,
+studyORwork  varchar(40) NOT NULL,) """
+    r="success"
+    except:
+        r="error"
+    return HttpResponse(r)
+    # return render(request, 'index.html')
 
 
 def db(request):
@@ -21,4 +41,3 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
-
