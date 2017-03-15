37009410a065782c5e63fbdcec1c7081bdc5e138
tanakorn.W <58460703006@dpu.ac.th>
2017-03-15 10:24:12 +0700

    test

--- hello/views.py
+++ hello/views.py
@@ -12,17 +12,18 @@ def index(request):
     try:
       conn = psycopg2.connect("dbname='ds7gk4tphvn2v' user='sfszejfztqdxyo' host='ec2-23-21-220-23.compute-1.amazonaws.com' password='9e2b53c8d90e3f9ad12e7764ac7ecee875724c6d2f1d3ae3d31517ac4d88bddc'")
       cur = conn.cursor()
+      sql_command =""" 
+    CREATE TABLE iotdpu_data(
+rank  integer NOT NULL,
+firstname  varchar(40) NOT NULL,
+lastname   varchar(40) NOT NULL,
+university  varchar(40) NOT NULL,
+degree   integer NOT NULL,
+department   varchar(40) NOT NULL,
+gpa   double NOT NULL,
+studyORwork  varchar(40) NOT NULL,) """
       r ="success"
-#     sql_command =""" 
-#     CREATE TABLE iotdpu_data(
-# rank  integer NOT NULL,
-# firstname  varchar(40) NOT NULL,
-# lastname   varchar(40) NOT NULL,
-# university  varchar(40) NOT NULL,
-# degree   integer NOT NULL,
-# department   varchar(40) NOT NULL,
-# gpa   double NOT NULL,
-# studyORwork  varchar(40) NOT NULL,) """
+    
     
     except:
         r ="error"
