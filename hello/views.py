7ad406db9ae190b61813b4812c2b979a9061c721
tanakorn.W <58460703006@dpu.ac.th>
2017-03-15 10:22:49 +0700

    test

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
-
+import psycopg2
 import requests
 
 # Create your views here.
@@ -10,7 +10,7 @@ import requests
 
 def index(request):
     try:
-      conn = psycopg2.connect("dbname='ds7gk4tphvn2v'user='sfszejfztqdxyo'host='ec2-23-21-220-23.compute-1.amazonaws.com'password='9e2b53c8d90e3f9ad12e7764ac7ecee875724c6d2f1d3ae3d31517ac4d88bddc'")
+      conn = psycopg2.connect("dbname='ds7gk4tphvn2v' user='sfszejfztqdxyo' host='ec2-23-21-220-23.compute-1.amazonaws.com' password='9e2b53c8d90e3f9ad12e7764ac7ecee875724c6d2f1d3ae3d31517ac4d88bddc'")
       cur = conn.cursor()
       r ="success"
 #     sql_command =""" 
